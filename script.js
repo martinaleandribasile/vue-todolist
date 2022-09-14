0b70457a07b65e66914271cdea3564d297dfaad7
MartinaBasile <martinaleandribasile@gmail.com>
2022-09-14 16:57:15 +0200

    file html css and js, ex complete with both bonus

---
+++ script.js
@@ -0,0 +1,52 @@
+const app = new Vue({
+    el: " #todosection ",
+    data: {
+        toDoItem: [
+            {
+                text: "Fare la spesa",
+                done: false,
+            },
+            {
+                text: "Pulire casa",
+                done: true,
+            },
+            {
+                text: "Prenotare ristorante",
+                done: false,
+            },
+            {
+                text: "Leggere un libro",
+                done: false,
+            },
+            {
+                text: "Portare a passeggio il cane",
+                done: true,
+            }
+        ],
+        inputToDo: ""
+    },
+    methods: {
+        changeDoneProp: function (index) {
+            this.toDoItem[index].done = !this.toDoItem[index].done
+        },
+        addToList: function () {
+            const action = this.inputToDo.trim();
+            const actionToDo = action[0].toUpperCase() + action.slice(1);
+            let newItme = {
+                text: actionToDo,
+                done: false
+            }
+            this.inputToDo = ""
+            this.toDoItem.push(newItme)
+        },
+        deleteItem(position) {
+            const newToDO = this.toDoItem.filter((element) => {
+                return element !== this.toDoItem[position]
+            })
+            this.toDoItem = newToDO
+            // this.toDoItem.splice(position, 1) -------altro metodo!
+        }
+    }
+})
+
+
